ab07b08faa68a55fdc297e6f353ddc6b47444b7b
Daniel H. Fuzari <dhfuzari@gmail.com>
2022-04-06 14:45:57 -0300

    refactor: create a categoriy details component to reduce responsabilities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { DevicesComponent } from './devices/devices.component';
+import { CategoryDetailComponent } from './category-detail/category-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, CategoriesComponent, DevicesComponent],
+  declarations: [AppComponent, CategoriesComponent, DevicesComponent, CategoryDetailComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
