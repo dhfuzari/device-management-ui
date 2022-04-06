b2f138da011160237481a0f33d23a21f7ea28a16
Daniel H. Fuzari <dhfuzari@gmail.com>
2022-04-06 15:24:15 -0300

    refactor: create a device details component to reduce responsibilities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { DevicesComponent } from './devices/devices.component';
 import { CategoryDetailComponent } from './category-detail/category-detail.component';
+import { DeviceDetailComponent } from './device-detail/device-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, CategoriesComponent, DevicesComponent, CategoryDetailComponent],
+  declarations: [AppComponent, CategoriesComponent, DevicesComponent, CategoryDetailComponent, DeviceDetailComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
