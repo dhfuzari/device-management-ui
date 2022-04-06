3c3ed783065a8980a12b89b6292a04c254fb7080
Daniel H. Fuzari <dhfuzari@gmail.com>
2022-04-06 18:24:16 -0300

    feat: device details and categories details routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Router, RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { DevicesComponent } from './devices/devices.component';
 import { CategoriesComponent } from './categories/categories.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DeviceDetailComponent } from './device-detail/device-detail.component';
+import { CategoryDetailComponent } from './category-detail/category-detail.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'devices', component: DevicesComponent },
   { path: 'categories', component: CategoriesComponent },
+  { path: 'device-detail/:id', component: DeviceDetailComponent },
+  { path: 'category-detail/:id', component: CategoryDetailComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 
