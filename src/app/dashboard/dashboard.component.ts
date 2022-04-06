fc4a64a08f84b81530e3ec50f1018cc08fa6df61
Daniel H. Fuzari <dhfuzari@gmail.com>
2022-04-06 17:30:55 -0300

    feat: creates dashboard component to show top 5 devices list

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Device } from '../device';
+import { DevicesService } from '../devices.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
+})
+export class DashboardComponent implements OnInit {
+  devices: Device[] = [];
+
+  constructor(private deviceService: DevicesService) {}
+
+  ngOnInit(): void {}
+
+  getDevices(): void {
+    this.deviceService
+      .getDevices()
+      .subscribe((devices) => (this.devices = devices.slice(1, 5)));
+  }
+}
