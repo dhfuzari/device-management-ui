a2f07e81a40ad76e3bd5a19eac415aea3cd6c442
Daniel H. Fuzari <dhfuzari@gmail.com>
2022-04-06 17:40:21 -0300

    feat: default dashboard route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Router, RouterModule, Routes } from '@angular/router';
 import { DevicesComponent } from './devices/devices.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'devices', component: DevicesComponent },
   { path: 'categories', component: CategoriesComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 
 @NgModule({
