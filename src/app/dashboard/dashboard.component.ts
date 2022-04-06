88596d27e1861d440a95c2f89d581eb93b987479
Daniel H. Fuzari <dhfuzari@gmail.com>
2022-04-06 17:51:43 -0300

    feat: fill top 5 devices and categories in dashboard component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Device } from '../device';
+import { Category } from '../category';
 import { DevicesService } from '../devices.service';
+import { CategoriesService } from '../categories.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,14 +11,27 @@ import { DevicesService } from '../devices.service';
 })
 export class DashboardComponent implements OnInit {
   devices: Device[] = [];
+  categories: Category[] = [];
 
-  constructor(private deviceService: DevicesService) {}
+  constructor(
+    private deviceService: DevicesService,
+    private categoriesService: CategoriesService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getDevices();
+    this.getCategories();
+  }
 
   getDevices(): void {
     this.deviceService
       .getDevices()
       .subscribe((devices) => (this.devices = devices.slice(1, 5)));
+  }
+
+  getCategories(): void {
+    this.categoriesService
+      .getCategories()
+      .subscribe((categories) => (this.categories = categories.slice(1, 5)));
   }
 }
