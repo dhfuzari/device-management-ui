63e3bd01d6c4fc2f0cc7fb5dcd636ccb54c73f82
Daniel H. Fuzari <dhfuzari@gmail.com>
2022-04-06 12:06:34 -0300

    chore: device and categories mocks created

---
+++ src/app/__mocks__/mock-categories.ts
@@ -0,0 +1,11 @@
+import { Category } from '../category';
+
+export const CATEGORIES: Category[] = [
+  { id: 1, name: 'smartphone' },
+  { id: 2, name: 'tablet' },
+  { id: 3, name: 'smartwatch' },
+  { id: 4, name: 'laptop' },
+  { id: 5, name: 'tv' },
+  { id: 6, name: 'earbud' },
+  { id: 7, name: 'speaker' },
+];
---
+++ src/app/__mocks__/mock-devices.ts
@@ -0,0 +1,27 @@
+import { Device } from '../device';
+
+export const CATEGORIES: Device[] = [
+  { id: 1, partNumber: 12341, color: 'green', categories_id: 1 },
+  { id: 2, partNumber: 12342, color: 'brown', categories_id: 2 },
+  { id: 3, partNumber: 12343, color: 'gray', categories_id: 3 },
+  { id: 4, partNumber: 12344, color: 'white', categories_id: 4 },
+  { id: 5, partNumber: 12345, color: 'yellow', categories_id: 5 },
+  { id: 6, partNumber: 12346, color: 'red', categories_id: 6 },
+  { id: 7, partNumber: 12347, color: 'pink', categories_id: 7 },
+
+  { id: 8, partNumber: 12348, color: 'green', categories_id: 1 },
+  { id: 9, partNumber: 12349, color: 'brown', categories_id: 2 },
+  { id: 10, partNumber: 12351, color: 'gray', categories_id: 3 },
+  { id: 11, partNumber: 12352, color: 'white', categories_id: 4 },
+  { id: 12, partNumber: 12353, color: 'yellow', categories_id: 5 },
+  { id: 13, partNumber: 12354, color: 'red', categories_id: 6 },
+  { id: 14, partNumber: 12355, color: 'pink', categories_id: 7 },
+
+  { id: 15, partNumber: 12356, color: 'green', categories_id: 1 },
+  { id: 16, partNumber: 12357, color: 'brown', categories_id: 2 },
+  { id: 17, partNumber: 12358, color: 'gray', categories_id: 3 },
+  { id: 18, partNumber: 12359, color: 'white', categories_id: 4 },
+  { id: 19, partNumber: 12360, color: 'yellow', categories_id: 5 },
+  { id: 20, partNumber: 12361, color: 'red', categories_id: 6 },
+  { id: 21, partNumber: 12362, color: 'pink', categories_id: 7 },
+];
