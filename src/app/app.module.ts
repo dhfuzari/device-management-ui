eba6603bca6f4962a8bed713bf46aa1ede229e77
Daniel H. Fuzari <dhfuzari@gmail.com>
2022-04-07 10:20:38 -0300

    chore: material-ui and cdk packages added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,14 @@ import { CategoryDetailComponent } from './category-detail/category-detail.compo
 import { DeviceDetailComponent } from './device-detail/device-detail.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -19,8 +27,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     CategoryDetailComponent,
     DeviceDetailComponent,
     DashboardComponent,
+    NavBarComponent,
   ],
-  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule],
   providers: [],
   bootstrap: [AppComponent],
 })
